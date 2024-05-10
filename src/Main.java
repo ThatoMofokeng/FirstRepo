6e9aafbc48f45e42ee8b0a78bd707468ad437c19
Thato Mofokeng <mofokengthato023@gmail.com>
2024-05-10 14:33:45 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Your Name: ");
+        String name = input.next();
+        System.out.println("Welcome" + name);
     }
 }
